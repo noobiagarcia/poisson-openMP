49efea8127db89d9ddcf650656ec668eed2cf34d
Nubia Garcia <157756799+noobiagarcia@users.noreply.github.com>
2024-02-05 11:38:12 -0300

    poisson_parallel.c

--- poisson_parallel.c
+++ poisson_parallel.c
@@ -4,7 +4,7 @@
 #include "omp.h"
 
 #define MAX 1e+10 //Numero máximo de iterações
-#define TOl 1e-13 //Tolerância do erro
+#define TOl 1e-6 //Tolerância do erro
 #define h  1e-4 //Tamanho do passo x
 #define k  1e-4 //Tamanho do passo y
 #define l 4400
@@ -15,32 +15,27 @@ void condcontorno(double **u, double **aux) //Inicializando as condições de co
 	int i, y1, y2, r, s, v, o;
 	double y, t;
 
-	for(i=c-(c*0.125); i<=c+(c*0.125); i++) //condição dos "is" marca o tamanho dos passos na malha
+	for(i=c-(c*0.125); i<=c+(c*0.125); i++)
 	{
 	    r = i+c*0.5;
-
 	    t = i*(8*M_PI/l);
 	    y = 0.125*l*sin(t); //equação da onda
 	    y1= (int) y + l*0.5;
 	    y2= (int) -1*y + l*0.5;
-
 	    u[y1][r] = -1;
 	    u[y2][r] = -1;
 	    aux[y1][r] = -1;
 		aux[y2][r] = -1;
 	}
-
 	for(i=0; i<(c*0.1); i++)
 	{
 	    s = i+c*0.1;
 	    v = (9*c*0.1)-i;
 	    o = c*0.1;
-
 	    u[o][s] = 1;
 	    u[s][o] = 1;
 	    u[c-(o)][v] = 1;
 	    u[v][c-(o)] = 1;
-
 	    aux[o][s] = 1;
 	    aux[s][o] = 1;
 	    aux[c-o][v] = 1;
@@ -51,30 +46,25 @@ double mtdffin(double **w, double **mt_aux)
 {
 	int i, j;
 	double  df, err = 0, r;
-
 #pragma omp parallel shared(mt_aux, df, w) private(i, j)
 {
-
 	for(i=1;i<l-1;i++)
 	{
 	    #pragma omp for
 		for(j=1;j<c-1;j++)
 		{
-			df = w[i][j]; //Salvando o valor atual da matriz no ponto [i][j]
+			df = w[i][j]; 
 			if(mt_aux[i][j] == 0)
 			{
-				w[i][j] = (-h*h + w[i+1][j] + w[i-1][j] + w[i][j-1] + w[i][j+1])*0.25; //Formula encontrada para calcular os pontos pelo metodo das diferenças finitas.
-
-				df = fabs(df - w[i][j]); //calculando o erro
+				w[i][j] = (-h*h + w[i+1][j] + w[i-1][j] + w[i][j-1] + w[i][j+1])*0.25; //metodo das diferenças finitas.
+				df = fabs(df - w[i][j]); 
 			}
 		}
 	}
 }
-	if(df > err) //Verificando se o erro é menor que o anterior.
-	{
+	if(df > err) //erro
 		err = df;
-	}
-
+	
 	return err;
 }
 void salvasol(double **w) //Salvando a solução no arquivo.
@@ -83,17 +73,12 @@ void salvasol(double **w) //Salvando a solução no arquivo.
 	int i, j;
 
 	fp = fopen("matriz_solucao.dat", "w");
-
 	for(i=0;i<l;i++)
 	{
 		for(j=0;j<c;j++)
-		{
 			fprintf(fp, "%lf\t", w[i][j]);
-		}
-
 		fprintf(fp,"\n");
 	}
-
  fclose(fp);
 }
 int main()
@@ -101,20 +86,16 @@ int main()
 	double **M, **Maux, er = 1;
 	int i, j, cont;
 
-	M = (double**)malloc(l*sizeof(double*)); //Alocando a matriz principal.
+	M = (double**)malloc(l*sizeof(double*)); //matriz principal.
 	for(i=0;i<l;i++)
-	{
-		M[i] = (double*)malloc(c*sizeof(double*));
+	{	M[i] = (double*)malloc(c*sizeof(double*));
 	}
 
-
-	Maux = (double**)malloc(l*sizeof(double*)); //Alocando a matriz auxiliar.
+	Maux = (double**)malloc(l*sizeof(double*)); //matriz auxiliar.
 	for(i=0;i<l;i++)
-	{
-		Maux[i] = (double*)malloc(c*sizeof(double*));
+	{	Maux[i] = (double*)malloc(c*sizeof(double*));
 	}
-
-	for(i=0;i<l;i++) //iniciandos as matrizes com zeros
+	for(i=0;i<l;i++) //iniciando
 	{
 		for(j=0;j<c;j++)
 		{
@@ -123,15 +104,12 @@ int main()
 		}
 	}
 
-	condcontorno(M, Maux); //Aplicando as condições de contorno as matrizes.
-
-	for(cont = 1; er>TOl && cont<MAX; cont++) //Aplicando o metodo, verificando se o erro está dentro da tolerancia e se o numero de iterações não foi exedido.
+	condcontorno(M, Maux);
+	for(cont = 1; er>TOl && cont<MAX; cont++) //Aplicando o metodo
 	{
 		er = mtdffin(M, Maux);
-
 		//printf("\nPasso: %d\n %.22lf\n", cont, er);
 	}
-
 	salvasol(M); //Salvando a solução
 
  return 0;
